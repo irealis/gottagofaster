5907a1fa34465995d38889ee78e01437805139ac
irealis <irealis@proton.me>
2023-12-17 20:47:49 +0100

    makes jumppad reset jumps
    collisions often dont quite extend up to the jumppad and it won't reset the count

--- src/jumppad.rs
+++ src/jumppad.rs
@@ -1,19 +1,23 @@
 use bevy::prelude::*;
 use bevy_xpbd_3d::prelude::{CollidingEntities, LinearVelocity};
 
-use crate::Player;
+use crate::{
+    character_controller::{Grounded, JumpCount},
+    Player,
+};
 
 #[derive(Component)]
 pub struct Jumppad(pub f32);
 
 pub fn apply_jumppad_boost(
-    mut player: Query<(Entity, &mut LinearVelocity), With<Player>>,
+    mut player: Query<(Entity, &mut LinearVelocity, &mut JumpCount), With<Player>>,
     pads: Query<(&CollidingEntities, &Jumppad)>,
 ) {
-    if let Ok((pe, mut linvel)) = player.get_single_mut() {
+    if let Ok((pe, mut linvel, mut jc)) = player.get_single_mut() {
         for (colliding, pad) in &pads {
             if colliding.contains(&pe) {
-                linvel.y += pad.0;
+                linvel.y = pad.0;
+                jc.0 = 0;
             }
         }
     }
